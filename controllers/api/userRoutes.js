dadc9f9870dc4d744ff34fc224216130841a2a85
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-03 13:12:18 -0800

    Login and sign up backend

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -44,4 +44,70 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            email: req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            password: req.body.password,
+            description: req.body.description,
+            gender: req.body.gender,
+            age: req.body.age
+        });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.username = newUser.email;
+            req.session.loggedIn = true;
+
+            res.json(newUser);
+        })
+    } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'No user account found!' });
+            return;
+        }
+
+        const isValid = user.checkPassword(req.body.password);
+
+        if (!isValid){
+            res.status(400).json({ message: 'No user account found!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.email;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'No user account found!' });
+      }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+});
+
 module.exports = router;
