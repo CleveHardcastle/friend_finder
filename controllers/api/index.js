c4fe7fd49f0988a6e85feacde9915bfca01aa765
Steph T <wytseng@uci.edu>
2023-01-03 09:53:06 -0800

    add api route to create and update a MESSAGE

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const messageRoutes = require('./messageRoutes');
+const roomRoutes = require('./roomRoutes');
+
+router.use('/message', messageRoutes);
+router.use('/room', roomRoutes);
+
+module.exports = router;
