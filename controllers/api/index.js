6ce95c8be639460fb12d4212cfb7b3f3bdffb076
talexander91 <116913084+talexander91@users.noreply.github.com>
2023-01-03 21:30:09 -0800

    working on room contoller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-const messageRoutes = require("./messageRoutes");
-const roomRoutes = require("./roomRoutes");
+// const messageRoutes = require("./messageRoutes");
+// const roomRoutes = require("./roomRoutes");
 
 
 const messageRoutes = require('./messageRoutes');
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
 const apiRoutes = require("./api");
 const profile = require("./profile");
+const room = require("./room.js");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/profile", profile);
+// router.use("/room", room);
 
 module.exports = router;
