262979f372e12874e8f76679688cd522dc051c19
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-05 10:26:20 -0800

    Added delete route for interest

--- controllers/api/interestRoutes.js
+++ controllers/api/interestRoutes.js
@@ -33,6 +33,24 @@ router.put('/:id', withAuth, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const affectedRows = Interest.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+    
+        if (affectedRows > 0) {
+            res.status(200).end();
+          } else {
+            res.status(404).end();
+          }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
