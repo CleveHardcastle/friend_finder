9a11b00351cb02b1dd65b688f03964b6b6988f6b
CleveHardcastle <treyhardcastle@gmail.com>
2023-01-04 19:51:47 -0800

    homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,22 +2,16 @@ const router = require("express").Router();
 
 const messageRoutes = require("./messageRoutes");
 const roomRoutes = require("./roomRoutes");
+const userRoutes = require("./userRoutes");
+const interestRoutes = require("./interestRoutes");
+const categoryRoutes = require("./categoryRoutes");
+const roomMemberRoutes = require("./roomMemberRoutes");
 
-
-const messageRoutes = require('./messageRoutes');
-const roomRoutes = require('./roomRoutes');
-const userRoutes = require('./userRoutes');
-const interestRoutes = require('./interestRoutes');
-const categoryRoutes = require('./categoryRoutes');
-const roomMemberRoutes = require('./roomMemberRoutes');
-
-
-router.use('/message', messageRoutes);
-router.use('/room', roomRoutes);
-router.use('/user', userRoutes);
-router.use('/interest', interestRoutes);
-router.use('/category', categoryRoutes);
-router.use('/room-member', roomMemberRoutes);
-
+router.use("/message", messageRoutes);
+router.use("/room", roomRoutes);
+router.use("/user", userRoutes);
+router.use("/interest", interestRoutes);
+router.use("/category", categoryRoutes);
+router.use("/room-member", roomMemberRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
 // import connection object
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 // const helpers = require('./utils/heplers')
-const routes = require('./controllers');
+const routes = require("./controllers");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     // login session expires in 10 minutes
     maxAge: 3600000,
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: false,
@@ -27,18 +27,16 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine','handlebars');
-app.use(express.urlencoded({ extended: true}));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "/public/images")));
 
 app.use(routes);
 
-
 // connect to db before starting server
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-  });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
