50032563c4fd0d22b2ee48c8705c72c12b1cbc3c
CleveHardcastle <treyhardcastle@gmail.com>
2023-01-06 14:45:08 -0800

    profile and edit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,27 +1,26 @@
-const router = require('express').Router();
-const { Room, User, roomMember } = require('../models');
 const router = require("express").Router();
 const { Room, User, roomMember } = require("../models");
 const room = require("./api/roomRoutes");
 
 router.get("/", async (req, res) => {
   try {
-    // If user is logged in, render room user is in 
+    // If user is logged in, render room user is in
     if (req.session.loggedIn) {
       const roomsData = await Room.findAll({
         attributes: ["id", "title", "description", "creator_id"],
-        include: [{ 
-          model: roomMember,
-          attributes: [],
-          where: {
-            member_id: req.session.userId
-          }
-        }],
+        include: [
+          {
+            model: roomMember,
+            attributes: [],
+            where: {
+              member_id: req.session.userId,
+            },
+          },
+        ],
       });
 
       const rooms = roomsData.map((room) => room.get({ plain: true }));
-      res.render('allRooms', { rooms });
-
+      res.render("allRooms", { rooms });
     } else {
       const roomsData = await Room.findAll({ include: User });
       const rooms = roomsData.map((room) => room.get({ plain: true }));
@@ -51,36 +50,36 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get('/dashboard', async (req, res) => {
+router.get("/dashboard", async (req, res) => {
   if (!req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
   } else {
     try {
       const roomData = await Room.findAll({
         include: User,
-        where: { creator_id: req.session.userId }
-      })
+        where: { creator_id: req.session.userId },
+      });
       const rooms = roomData.map((room) => room.get({ plain: true }));
-      
-      res.render('dashboard', { rooms });
+
+      res.render("dashboard", { rooms });
     } catch (err) {
       res.status(500).json(err);
     }
   }
 });
 
-router.get('/dashboard', async (req, res) => {
+router.get("/dashboard", async (req, res) => {
   if (!req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
   } else {
     try {
       const roomData = await Room.findAll({
         include: User,
-        where: { creator_id: req.session.userId }
-      })
+        where: { creator_id: req.session.userId },
+      });
       const rooms = roomData.map((room) => room.get({ plain: true }));
-      
-      res.render('dashboard', { rooms });
+
+      res.render("dashboard", { rooms });
     } catch (err) {
       res.status(500).json(err);
     }
