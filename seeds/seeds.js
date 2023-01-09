c5919d26904119352d565d7334d889dc32211e1f
Steph T <wytseng@uci.edu>
2023-01-08 16:43:56 -0800

    remove all seed files beside category

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,20 +1,11 @@
 const { User, Room, Message, Interest, Category, roomMember } = require('../models');
 
-
-const userData = require('./userData.json');
-const roomData = require('./roomData.json');
-const roomMemberData = require('./roomMember.json');
 const sequelize = require('../config/connection');
 
 const seedDatabase = async() => {
   await sequelize.sync({force: true});
 
-  const users = await User.bulkCreate(userData);
-  const rooms = await Room.bulkCreate(roomData);
-  const roomMembers = await roomMember.bulkCreate(roomMemberData);
   const categories = await Category.bulkCreate(require('./categoryData.json'));
-  const messages = await Message.bulkCreate(require('./messageData.json'));
-  const interests = await Interest.bulkCreate(require('./interestData.json'));
 
   process.exit(0);
 };
